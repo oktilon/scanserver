fa61002714e96536174ec70fdaccbd0dcedb1e82
Denis <oktilon@gmail.com>
2019-06-19 11:11:46 +0300

    license

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
 'use strict';
 
 const express = require('express');
+const config = require('./config.json');
+const defaultConfig = config.development;
+const environment = process.env.NODE_ENV || 'development';
+const environmentConfig = config[environment];
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
+
+const postData = querystring.stringify({
+  'msg': 'Hello World!'
+});
+
+const options = {
+    protocol: 'https',
+    hostname: 'dnk-bilet.com',
+    port: 443,
+    path: '/api/get_events',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Length': Buffer.byteLength(postData)
+    }
+};
+
+const req = http.request(options, (res) => {
+  console.log(`STATUS: ${res.statusCode}`);
+  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+  res.setEncoding('utf8');
+  res.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`);
+  });
+  res.on('end', () => {
+    console.log('No more data in response.');
+  });
+});
+
+req.on('error', (e) => {
+  console.error(`problem with request: ${e.message}`);
+});
+
+// write data to request body
+req.write(postData);
+req.end();
+
 
 // App
 const app = express();
